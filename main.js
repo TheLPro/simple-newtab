988b97da1f73ab7c7a686de9e884cf2f6d332d6e
Adam Buksowicz <gammert54@gmail.com>
2024-08-24 21:50:56 +0200

    Added Better Search Capabilites and close button

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 var form = document.getElementById('form');
 var urlinput = document.getElementById('urlinp');
 var searchbutton = document.getElementById('searchbtn');
+var closepopupbutton = document.getElementById('closepopup');
 
 var settings = document.getElementById('settings');
 var popup = document.getElementById('popup');
 var primarycolor = document.getElementById('primarycolor');
 var secondarycolor = document.getElementById('secondarycolor');
 
-popup.style.display = 'none';
+closepopupbutton.addEventListener('click', function () {
+  popup.classList.remove('active');
+});
+
+settings.addEventListener('click', function () {
+  popup.classList.toggle('active');
+});
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
   console.log(urlinput.value);
+  if (urlinput.value === '') {
+    return false;
+  }
+
+  if (
+    urlinput.value.includes('http://') ||
+    urlinput.value.includes('https://')
+  ) {
+    window.location.href = urlinput.value;
+    return true;
+  }
 
   window.location.href = `https://www.google.com/search?q=${urlinput.value}`;
 
